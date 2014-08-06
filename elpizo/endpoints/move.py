c189e4a196432f5563db8e980d260a205a10e62d
Tony Young <tony@rfw.name>
2014-08-07 02:27:58 +1000

    always send movement packets

--- elpizo/endpoints/move.py
+++ elpizo/endpoints/move.py
@@ -14,48 +14,49 @@ def get_direction_vector(d):
 
 
 def socket_move(ctx, message):
-  direction = message.direction
+  try:
+    direction = message.direction
 
-  dax, day = get_direction_vector(direction)
-  ctx.player.direction = direction
-  ctx.sqla.commit()
+    dax, day = get_direction_vector(direction)
+    ctx.player.direction = direction
+    ctx.sqla.commit()
 
-  ctx.player.ax += dax
-  ctx.player.ay += day
+    ctx.player.ax += dax
+    ctx.player.ay += day
 
-  region = ctx.player.region
+    region = ctx.player.region
 
-  if region is None:
-    ctx.sqla.rollback()
-    return
+    if region is None:
+      ctx.sqla.rollback()
+      return
 
-  corners = [
-      region.corners[(ctx.player.ry + 0) * (Region.SIZE + 1) +
-                     (ctx.player.rx + 0)],
-      region.corners[(ctx.player.ry + 0) * (Region.SIZE + 1) +
-                     (ctx.player.rx + 1)],
-      region.corners[(ctx.player.ry + 1) * (Region.SIZE + 1) +
-                     (ctx.player.rx + 0)],
-      region.corners[(ctx.player.ry + 1) * (Region.SIZE + 1) +
-                     (ctx.player.rx + 1)]
-  ]
-  terrain_map = {terrain.id: terrain
-                 for terrain
-                 in ctx.sqla.query(Terrain).filter(Terrain.id.in_(corners))}
+    corners = [
+        region.corners[(ctx.player.ry + 0) * (Region.SIZE + 1) +
+                       (ctx.player.rx + 0)],
+        region.corners[(ctx.player.ry + 0) * (Region.SIZE + 1) +
+                       (ctx.player.rx + 1)],
+        region.corners[(ctx.player.ry + 1) * (Region.SIZE + 1) +
+                       (ctx.player.rx + 0)],
+        region.corners[(ctx.player.ry + 1) * (Region.SIZE + 1) +
+                       (ctx.player.rx + 1)]
+    ]
+    terrain_map = {terrain.id: terrain
+                   for terrain
+                   in ctx.sqla.query(Terrain).filter(Terrain.id.in_(corners))}
 
-  if sum([terrain_map[corner].passable for corner in corners]) <= 1:
-    ctx.sqla.rollback()
-    return
+    if sum([terrain_map[corner].passable for corner in corners]) <= 1:
+      ctx.sqla.rollback()
+      return
 
-  if ctx.sqla.query(ctx.sqla.query(Fixture).filter(
-      Fixture.bbox_contains(ctx.player.realm_id, ctx.player.ax, ctx.player.ay)
-  ).exists()).scalar():
-    ctx.sqla.rollback()
-    return
+    if ctx.sqla.query(ctx.sqla.query(Fixture).filter(
+        Fixture.bbox_contains(ctx.player.realm_id, ctx.player.ax, ctx.player.ay)
+    ).exists()).scalar():
+      ctx.sqla.rollback()
+      return
 
-  ctx.sqla.commit()
-  ctx.publish(ctx.player.region.routing_key,
-              game_pb2.Packet.MOVE, message)
+    ctx.sqla.commit()
+  finally:
+    ctx.publish(ctx.player.region.routing_key, game_pb2.Packet.MOVE, message)
 
 
 def mq_move(ctx, origin, message):
