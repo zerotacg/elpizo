e15977e692fd5607e9a3638836964c29b31eaa55
Tony Young <tony@rfw.name>
2014-08-08 01:14:09 +1000

    add movement unjank factor

--- elpizo/endpoints/move.py
+++ elpizo/endpoints/move.py
@@ -21,7 +21,7 @@ def socket_move(ctx, message):
 
   dt = now - last_move_time
 
-  if dt < 1 / ctx.player.speed:
+  if dt < 1 / ctx.player.speed * 1.5:  # compensate for slow connections by 1.5
     ctx.send(ctx.player.to_origin_protobuf(), game_pb2.TeleportPacket(
         location=ctx.player.location_to_protobuf(),
         direction=ctx.player.direction))
