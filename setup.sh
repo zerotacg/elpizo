4be91fbafe1244f54f693d790be956a2271469b1
Tony Young <tony@rfw.name>
2014-08-08 01:58:18 +1000

    create the postgres database

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt-get install -y postgresql-server-dev-9.3 libpython3.4-dev libxml2-dev \
                         libxslt1-dev cython3 nodejs-legacy python3.4 \
                         rabbitmq-server nodejs python3-setuptools npm \
-                        git build-essential libtool autoconf && \
+                        git build-essential libtool autoconf postgresql-9.3 && \
 sudo easy_install-3.4 pip && \
 sudo pip3.4 install virtualenv && \
 sudo npm install -g bower && \
@@ -16,4 +16,7 @@ bower install && \
 openssl genrsa -out elpizo.pem 2048 && \
 openssl rsa -in elpizo.pem -pubout -out elpizo.pub && \
 node_modules/.bin/gulp protos && \
+sudo -u postgres psql -c "CREATE USER elpizo WITH PASSWORD 'elpizo';" && \
+sudo -u postgres createdb elpizo -O elpizo && \
+echo "dsn = \"postgres://elpizo:elpizo@localhost/elpizo\"" > elpizo.conf && \
 python -m elpizo.tools.initdb
