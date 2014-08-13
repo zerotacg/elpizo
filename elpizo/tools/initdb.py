2acc08dd55f0603ec7dbe6e7047aaef0e4e37538
Tony Young <tony@rfw.name>
2014-08-14 09:42:05 +1000

    Revert "add complex terrain rendering (needs interpolation"
    This reverts commit 128999d874b46b8bf88e4bbcfcbaf8745a933867.

--- elpizo/tools/initdb.py
+++ elpizo/tools/initdb.py
@@ -41,12 +41,7 @@ def initialize_realm(app):
     for arx in range(realm.aw // Region.SIZE):
       region = Region(arx=arx, ary=ary, realm=realm,
                       tiles=[random.choice([grassland, grassland]).id
-                             for _ in range(16 * 16)],
-                      heightmap=[0] * (16 * 16))
-      region.heightmap[1] = 2
-      region.heightmap[2] = 2
-      region.heightmap[3] = 2
-      region.heightmap[4] = 1
+                             for _ in range(16 * 16)])
       sqla.add(region)
 
   sqla.commit()
