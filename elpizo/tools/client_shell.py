f49d35397c25a275525c9a85b326c3dbc17750d4
Tony Young <tony@rfw.name>
2014-08-24 02:30:15 +1000

    get rid of ws4py dependency

--- elpizo/tools/client_shell.py
+++ elpizo/tools/client_shell.py
@@ -1,38 +1,68 @@
+import asyncio
 import IPython
 import logging
 import os
 import sys
+import threading
+import websockets
 
 from elpizo.protos import packets_pb2
+from elpizo.util import green
 from elpizo.util import mint
 from elpizo.util import net
 
-from ws4py.client import threadedclient
 from urllib.parse import urlencode
 
 
-class GameClient(threadedclient.WebSocketClient):
+class GameClient(threading.Thread):
+  daemon = True
+
   def __init__(self, token, *args, **kwargs):
     super().__init__(*args, **kwargs)
     self.token = token
+
+  def run(self):
+    self.loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(self.loop)
+    self.loop.run_until_complete(self._run())
+
+  @green.coroutine
+  def _run(self):
+    self.websocket = green.await_coro(
+        websockets.connect("ws://localhost:8765/socket"))
+
+    self.opened()
+    try:
+      while True:
+        packet = green.await_coro(self.websocket.recv())
+        if packet is None:
+          break
+        self.received_message(packet)
+    finally:
+      self.closed()
 
   def opened(self):
     logging.info("Connection opened.")
     self.send(packets_pb2.HelloPacket(token=self.token))
 
-  def closed(self, code, reason):
+  def closed(self):
     logging.info("Connection closed.")
     os._exit(1)
 
-  def received_message(self, m):
-    origin, message = net.Protocol.deserialize_packet(m.data)
+  def received_message(self, packet):
+    origin, message = net.Protocol.deserialize_packet(packet)
 
     print(type(message).__name__)
     print(origin)
     print(message)
 
+  @green.coroutine
+  def _send(self, message):
+    green.await_coro(
+        self.websocket.send(net.Protocol.serialize_packet(None, message)))
+
   def send(self, message):
-    super().send(net.Protocol.serialize_packet(None, message), binary=True)
+    self.loop.call_soon_threadsafe(self._send, message)
 
 
 def main():
@@ -49,8 +79,8 @@ def main():
 
   logging.basicConfig(level=logging.INFO)
 
-  ws = GameClient(token, "ws://localhost:8765/socket")
-  ws.connect()
+  ws = GameClient(token)
+  ws.start()
 
   IPython.embed(user_ns={"ws": ws, "pb": packets_pb2})
 
