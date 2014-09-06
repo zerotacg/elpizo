4291eaa357b8ab56b27550cd65e5f255a5743326
Tony Young <tony@rfw.name>
2014-09-06 22:27:28 +1000

    get rid of to/from_protobuf

--- elpizo/models/items/equipment/weapons.py
+++ elpizo/models/items/equipment/weapons.py
@@ -5,17 +5,6 @@ from elpizo.protos import items_pb2
 class Weapon(equipment.Equipment):
   SLOT = 4
 
-  def to_protobuf(self):
-    proto = super().to_protobuf()
-    proto.Extensions[items_pb2.Weapon.ext].MergeFrom(items_pb2.Weapon())
-    return proto
-
-  @classmethod
-  def from_protobuf(cls, proto):
-    record = super().from_protobuf(proto)
-    proto = proto.Extensions[items_pb2.Weapon.ext]
-    return record
-
 
 class Dagger(Weapon):
   TYPE = "dagger"
