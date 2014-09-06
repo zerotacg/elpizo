be6a387f2c982b60988812b25d823cbc98ffeb60
Tony Young <tony@rfw.name>
2014-09-06 22:27:59 +1000

    ... and in Equipment

--- elpizo/models/items/equipment/equipment.py
+++ elpizo/models/items/equipment/equipment.py
@@ -3,13 +3,4 @@ from elpizo.protos import items_pb2
 
 
 class Equipment(items.Item):
-  def to_protobuf(self):
-    proto = super().to_protobuf()
-    proto.Extensions[items_pb2.Equipment.ext].MergeFrom(items_pb2.Equipment())
-    return proto
-
-  @classmethod
-  def from_protobuf(cls, proto):
-    record = super().from_protobuf(proto)
-    proto = proto.Extensions[items_pb2.Equipment.ext]
-    return record
+  pass
