6d5720e89a1a8add74bd95e13073a95c3aa7fc9c
Tony Young <tony@rfw.name>
2014-09-10 21:20:54 +1000

    try fix travis build again

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 apt-get update
-apt-get install -y libpython3.4-dev python3.4 nodejs python3-setuptools npm git \
-                   build-essential libtool autoconf redis-server nginx
+apt-get install -y libpython3.4-dev python3.4 nodejs python3-setuptools npm \
+                   git build-essential libtool autoconf redis-server nginx
 ln -s /usr/bin/nodejs /usr/bin/node
-easy_install-3.4 pip
+easy_install3 pip
 npm install -g bower gulp
 git clone https://github.com/GreatFruitOmsk/protobuf-py3.git
 cd protobuf-py3
--- deploy-bootstrap.sh
+++ deploy-bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo ln -s . /vagrant
 sudo add-apt-repository -y ppa:fkrull/deadsnakes
+sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo ./bootstrap.sh
